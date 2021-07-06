94fab1860afa319dce32f77cb4a060f6fc5a9a6f
Bogdan Niedzwiecki <bogdan.niedzwiecki@possible.com>
2021-07-06 17:19:10 +0200

    refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -9,88 +9,194 @@ class App extends Component {
   state = {
     output: "",
     input: "0",
+    answer: "",
   };
 
-  handleButtonClick = (sign, type) => {
-    if (type === "digit") {
-      if (this.state.input.match(/^[0]/) && !this.state.input.match(/[.]/)) {
-        this.setState(() => ({
+  isInputZero = () => {
+    return this.state.input === "0" || this.state.input === "-0";
+  };
+
+  isInputDecimal = () => {
+    return this.state.input.indexOf(".") !== -1;
+  };
+
+  isInputOperator = () => {
+    return this.state.input.match(/^[+-/*//]$/);
+  };
+
+  isOutputEmpty = () => {
+    return this.state.output === "";
+  };
+
+  isOutputZeroOrEmpty = () => {
+    return this.state.output === "0" || this.state.output === "";
+  };
+
+  isOutputEndsWithDigital = () => {
+    return this.state.output.match(/\d$/);
+  };
+
+  isOutputEndsWithDivideOrMultiply = () => {
+    return this.state.output.match(/[*/]$/);
+  };
+
+  isOutputEndsWithDivideOrMultiplyAndMinus = () => {
+    return this.state.output.match(/[*/][-]$/);
+  };
+
+  isOutputHasOperator = () => {
+    return this.state.output.match(/[*/+-]/);
+  };
+
+  isAnswer = () => {
+    return this.state.answer;
+  };
+
+  onClear = () => {
+    this.setState(() => ({
+      output: "",
+      input: "0",
+    }));
+  };
+
+  onDigit = (sign) => {
+    if (this.state.answer) {
+      this.setState((state) => ({
+        ...state,
+        output: sign,
+        input: sign,
+        answer: "",
+      }));
+    } else {
+      if (this.isInputZero() && this.isOutputZeroOrEmpty()) {
+        this.setState((state) => ({
+          ...state,
           output: sign,
           input: sign,
         }));
       } else if (
-        this.state.input.match(/^[+-/*//]/) &&
-        !this.state.input.match(/[.]/)
+        this.isInputOperator() &&
+        !this.isOutputEndsWithDivideOrMultiplyAndMinus()
       ) {
         this.setState((state) => ({
+          ...state,
           output: state.output + sign,
           input: sign,
         }));
-      } else {
+      } else if (!this.isInputZero()) {
         this.setState((state) => ({
+          ...state,
           output: state.output + sign,
           input: state.input + sign,
         }));
-      }
-    } else if (type === "operator" && this.state.output.length) {
-      if (this.state.output.match(/[+-/*//]$/)) {
-        if (sign === "-" && !this.state.output.match(/[+-]$/)) {
-          this.setState((state) => ({
-            output: state.output + sign,
-            input: sign,
-          }));
-        } else if (this.state.output.match(/[/*//][-]$/)) {
-          this.setState((state) => ({
-            output: state.output.slice(0, -2) + sign,
-            input: sign,
-          }));
-        } else {
-          this.setState((state) => ({
-            output: state.output.replace(/.$/, sign),
-            input: sign,
-          }));
-        }
-      } else if (this.state.output.match(/[=]/)) {
-        this.setState((state) => ({
-          output: state.output.slice(state.output.indexOf("=") + 1) + sign,
-          input: sign,
-        }));
       } else {
         this.setState((state) => ({
-          output: state.output + sign,
+          ...state,
+          output: state.output.slice(0, -1) + sign,
           input: sign,
         }));
       }
-    } else if (
-      type === "answer" &&
-      this.state.output.match(/[+-/*//]/) &&
-      !this.state.output.match(/[+-/*//]$/) &&
-      !this.state.output.match(/[=]/)
-    ) {
-      this.setState((state) => {
-        const answer = eval(state.output);
-        return {
-          output: state.output + "=" + answer,
-          input: answer,
-        };
-      });
-    } else if (type === "clear") {
-      this.setState(() => ({
-        output: "",
-        input: "0",
+    }
+  };
+
+  onDecimal = (sign) => {
+    if (this.state.answer) {
+      this.setState((state) => ({
+        ...state,
+        output: state.answer + sign,
+        input: state.input + sign,
+        answer: "",
       }));
-    } else if (type === "decimal") {
-      if (!this.state.output.length) {
+    } else {
+      if (this.isOutputEmpty()) {
         this.setState((state) => ({
+          ...state,
           output: "0" + sign,
           input: state.input + sign,
         }));
-      } else if (!this.state.output.match(/[.]$/)) {
+      } else if (!this.isInputDecimal() && !this.isInputOperator()) {
         this.setState((state) => ({
+          ...state,
           output: state.output + sign,
           input: state.input + sign,
         }));
       }
+    }
+  };
+
+  onOperator = (sign) => {
+    if (this.state.answer) {
+      this.setState((state) => ({
+        ...state,
+        output: state.answer + sign,
+        input: sign,
+        answer: "",
+      }));
+    } else {
+      if (this.isOutputEndsWithDigital()) {
+        this.setState((state) => ({
+          ...state,
+          output: state.output + sign,
+          input: sign,
+        }));
+      } else if (sign === "-" && this.isOutputEndsWithDivideOrMultiply()) {
+        this.setState((state) => ({
+          ...state,
+          output: state.output + sign,
+          input: sign,
+        }));
+      } else if (this.isOutputEndsWithDivideOrMultiplyAndMinus()) {
+        this.setState((state) => ({
+          ...state,
+          output: state.output.slice(0, -2) + sign,
+          input: sign,
+        }));
+      } else {
+        this.setState((state) => ({
+          ...state,
+          output: state.output.slice(0, -1) + sign,
+          input: sign,
+        }));
+      }
+    }
+  };
+
+  onEquals = () => {
+    if (
+      this.isOutputEndsWithDigital() &&
+      this.isOutputHasOperator() &&
+      !this.isAnswer()
+    ) {
+      this.setState((state) => {
+        const answer = Math.round(eval(state.output) * 100) / 100;
+        return {
+          output: state.output + "=" + answer,
+          input: answer,
+          answer: answer,
+        };
+      });
+    }
+  };
+
+  handleButtonClick = (sign, type) => {
+    switch (type) {
+      case "clear":
+        this.onClear();
+        break;
+      case "digit":
+        this.onDigit(sign);
+        break;
+      case "operator":
+        this.onOperator(sign);
+        break;
+      case "decimal":
+        this.onDecimal(sign);
+        break;
+      case "equals":
+        this.onEquals();
+        break;
+      default:
+        console.log("Sorry, we are out of " + type + ".");
     }
   };
 
